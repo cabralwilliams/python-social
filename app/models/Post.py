2c2670971bbea6741bb6e544f91c2c240600ebc7
Cabral Williams <cabral.williams@gmail.com>
2022-02-08 16:06:00 -0600

    Added the Vote model

--- app/models/Post.py
+++ app/models/Post.py
@@ -1,8 +1,9 @@
 # Import the Base class
 from app.db import Base
-from sqlalchemy import Integer, Column, String, ForeignKey, DateTime
+from sqlalchemy import Integer, Column, String, ForeignKey, DateTime, select, func, and_
 from datetime import datetime
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, column_property
+from .Vote import Vote
 
 class Post(Base):
     __tablename__ = 'posts'
@@ -12,6 +13,22 @@ class Post(Base):
     created_at = Column(DateTime, default=datetime.now)
     updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
     user_id = Column(Integer, ForeignKey('users.id'))
+    up_votes = column_property(
+        select([func.count(Vote.id)]).where(
+            and_(
+                Vote.post_id == id,
+                Vote.is_upvote == True
+            )
+        )
+    )
+    down_votes = column_property(
+        select([func.count(Vote.id)]).where(
+            and_(
+                Vote.post_id == id,
+                Vote.is_upvote == False
+            )
+        )
+    )
 
     user = relationship('User')
     # Remove comments associated with post upon deletion of post
